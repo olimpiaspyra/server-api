8ef3f7608c21c2a19583ed9605b1da63c077e066
Olimpia Spyra <olimpia.spyra@gmail.com>
2021-08-28 21:47:45 +0200

    Add heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
 const seatsRoutes = require('./routes/seats.routes');
+const path = require('path');
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
@@ -12,11 +13,16 @@ app.use(cors());
 app.use('/api/', testimonialsRoutes);
 app.use('/api/', concertsRoutes);
 app.use('/api/', seatsRoutes);
+app.use(express.static(path.join(__dirname, '/client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 app.use((req, res) => {
   res.status(404).send('404 not found...');
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
