119d023ba990482991840783ea2f763e692a485c
Olimpia Spyra <olimpia.spyra@gmail.com>
2021-10-05 19:34:22 +0200

    Fix heroku

--- server.js
+++ server.js
@@ -37,9 +37,8 @@ app.use((req, res) => {
 const NODE_ENV = process.env.NODE_ENV;
 let dbUri = '';
 
-if(NODE_ENV === 'production') dbUri = 'url to remote db';
+if(NODE_ENV === 'production') dbUri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.kwezu.mongodb.net/NewWaveDB?retryWrites=true&w=majority`;
 else if(NODE_ENV === 'test') dbUri = 'mongodb://localhost:27017/NewWaveDBtest';
-else dbUri = `mongodb+srv://${process.env.USERNAME}:${process.env.NewWaveDB}@cluster0.kwezu.mongodb.net/NewWaveDB?retryWrites=true&w=majority`;
 
 mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
